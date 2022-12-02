9a8df2c04d2516809077d32e3c2a37478f87fea8
Frederic Stadler <frederic23.mai@gmail.com>
2022-12-02 11:14:38 +0100

    Day 2

---
+++ 2022/Day2/part2/index.js
@@ -0,0 +1,87 @@
+/*
+Author: Fredwuz (frederic23.mai@gmail.com)
+index.js (c) 2022
+Desc: description
+Created:  2.12.2022
+Modified: 2.12.2022
+*/
+
+let fs = require('fs');
+
+(async () => {
+  let data = await fs.readFileSync('input.txt', 'utf8');
+  let lines = data.split('\n');
+  let rock = 1,
+    paper = 2,
+    scissors = 3,
+    win = 6,
+    draw = 3,
+    lose = 0,
+    score = 0;
+
+  /*
+  win = 6
+  draw = 3
+  lose = 0
+  Rock = 1
+  Paper = 2
+  Scissors = 3
+  Choice
+    Lose = X
+    Draw = Y
+    Win = Z
+  Player
+    Rock = X
+    Paper = Y
+    Siccors = Z
+  Elves
+    Rock = A
+    Paper = B
+    Siccors = C
+
+  */
+  let choices = {
+    X: {
+      A: 'Z',
+      B: 'X',
+      C: 'Y',
+    },
+    Y: {
+      A: 'X',
+      B: 'Y',
+      C: 'Z',
+    },
+    Z: {
+      A: 'Y',
+      B: 'Z',
+      C: 'X',
+    },
+  };
+
+  for (let i = 0; i < lines.length; i++) {
+    let input = lines[i].split(' ');
+    let elves = input[0];
+    let choice = input[1];
+
+    let player = choices[choice][elves];
+
+    switch (player) {
+      case 'X':
+        score += rock;
+        break;
+      case 'Y':
+        score += paper;
+        break;
+      case 'Z':
+        score += scissors;
+        break;
+    }
+
+    if ((player == 'X' && elves == 'C') || (player == 'Y' && elves == 'A') || (player == 'Z' && elves == 'B')) {
+      score += win;
+    } else if ((player == 'X' && elves == 'A') || (player == 'Y' && elves == 'B') || (player == 'Z' && elves == 'C')) {
+      score += draw;
+    }
+  }
+  console.log(score);
+})();
